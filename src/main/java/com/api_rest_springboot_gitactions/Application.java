bf04f326d647a3afc099739b6592f35a5d602802
VeronicaRG-asamitaka <veronica.ruizgalvan@hotmail.com>
2026-03-16 21:46:27 -0600

    Se agrega proyecto spring a repositorio

---
+++ src/main/java/com/api_rest_springboot_gitactions/Application.java
@@ -0,0 +1,13 @@
+package com.api_rest_springboot_gitactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_rest_springboot_gitactions/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_rest_springboot_gitactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
